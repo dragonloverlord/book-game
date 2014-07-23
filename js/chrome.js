e871e11b2b354a05d1a44d96704c1a353eda8d5a
dragonloverlord <dragonloverlord@gmail.com>
2014-07-23 19:03:44 -0400

    Fragmented the book.js file.

---
+++ js/chrome.js
@@ -0,0 +1,78 @@
+document.addEventListener('DOMContentLoaded', function () {
+  chrome.storage.sync.get('money',function(obj) {
+    var money = parseInt(obj.money);
+    gt("money-text").innerHTML = money;
+  });
+  chrome.storage.sync.get('drama',function(obj) {
+    var drama = parseInt(obj.drama);
+    gt("drama-stats").innerHTML = drama;
+  });
+  chrome.storage.sync.get('fantasy',function(obj) {
+    var fantasy = parseInt(obj.fantasy);
+    gt("fantasy-stats").innerHTML = fantasy;
+  });
+  chrome.storage.sync.get('scifi',function(obj) {
+    var scifi = parseInt(obj.scifi);
+    gt("scifi-stats").innerHTML = scifi;
+  });
+  chrome.storage.sync.get('trashy',function(obj) {
+    var trashy = parseInt(obj.trashy);
+    gt("trashy-stats").innerHTML = trashy;
+  });
+  chrome.storage.sync.get('History',function(obj) {
+    var history = parseInt(obj.History);
+    gt("history-stats").innerHTML = history;
+  });
+  chrome.storage.sync.get('childrens',function(obj) {
+    var childrens = parseInt(obj.childrens);
+    gt("childrens-stats").innerHTML = childrens;
+  });
+  chrome.storage.sync.get('trashyLock',function(obj) {
+    var trashyLock = parseInt(obj.trashyLock);
+    gt("trashy").value = trashyLock;
+  });
+  chrome.storage.sync.get('historyLock',function(obj) {
+    var historyLock = parseInt(obj.historyLock);
+    gt("history").value = historyLock;
+  });
+  chrome.storage.sync.get('romanceLock',function(obj) {
+    var romanceLock = parseInt(obj.romanceLock);
+    gt("romance").value = romanceLock;
+  });
+  chrome.storage.sync.get('fiction',function(obj) {
+    var fiction = parseInt(obj.fiction);
+    gt("fiction-stats").innerHTML = fiction;
+  });
+  chrome.storage.sync.get('nonfiction',function(obj) {
+    var nonfiction = parseInt(obj.nonfiction);
+    gt("nonfiction-stats").innerHTML = nonfiction;
+  });
+  chrome.storage.sync.get('math',function(obj) {
+    var math = parseInt(obj.math);
+    gt("math-stats").innerHTML = math;
+  });
+  chrome.storage.sync.get('humor',function(obj) {
+    var humor = parseInt(obj.humor);
+    gt("humor-stats").innerHTML = humor;
+  });
+  chrome.storage.sync.get('satire',function(obj) {
+    var satire = parseInt(obj.satire);
+    gt("satire-stats").innerHTML = satire;
+  });
+  chrome.storage.sync.get('romance',function(obj) {
+    var romance = parseInt(obj.romance);
+    gt("romance-stats").innerHTML = romance;
+  });
+  chrome.storage.sync.get('report',function(obj) {
+    var report = parseInt(obj.report);
+    gt("report-stats").innerHTML = report;
+  });
+  chrome.storage.sync.get('survival',function(obj) {
+    var survival = parseInt(obj.survival);
+    gt("survival-stats").innerHTML = survival;
+  });
+  chrome.storage.sync.get('gothic',function(obj) {
+    var gothic = parseInt(obj.gothic);
+    gt("gothic-stats").innerHTML = gothic;
+  });
+});
---
+++ js/reset.js
@@ -0,0 +1,33 @@
+function reset(){
+  chrome.storage.sync.clear();
+  idf("money-text").innerHTML = 0;
+  idf("childrens-stats").innerHTML = 0;
+  idf("drama-stats").innerHTML = 0;
+  idf("fantasy-stats").innerHTML = 0;
+  idf("fiction-stats").innerHTML = 0;
+  idf("history-stats").innerHTML = 0;
+  idf("scifi-stats").innerHTML = 0;
+  idf("trashy-stats").innerHTML = 0;
+  idf("nonfiction-stats").innerHTML = 0;
+  idf("math-stats").innerHTML = 0;
+  idf("humor-stats").innerHTML = 0;
+  idf("satire-stats").innerHTML = 0;
+  idf("romance-stats").innerHTML = 0;
+  idf("report-stats").innerHTML = 0;
+  idf("survival-stats").innerHTML = 0;
+  idf("gothic-stats").innerHTML = 0;
+  var fantasy = idf("fantasy");
+  fantasy.innerHTML = "Locked ???";
+  fantasy.className = "book-button-locked";
+  var satire = idf("satire");
+  satire.innerHTML = "Locked ???";
+  satire.className = "book-button-locked";
+  var history = idf("history");
+  history.innerHTML = "Locked 1500$";
+  history.value = 0;
+  history.className = "book-button-locked";
+  var trashy = idf("trashy");
+  trashy.innerHTML = "Locked 1000$";
+  trashy.value = 0;
+  trashy.className = "book-button-locked";
+}
