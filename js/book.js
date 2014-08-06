a8d51e289dc9f2d707a83eba06163303cf70caf1
dragonloverlord <dragonloverlord@gmail.com>
2014-08-06 17:06:23 -0400

    Reorganized

--- js/book.js
+++ js/book.js
@@ -1,19 +1,3 @@
-function bookPrompt(){
-  document.getElementById('iframe').src = 'html/prompt.html';
-}
-
-function errorPopup(){
-  document.getElementById('iframe').src = 'html/error.html';
-}
-
-function lockedPopup(){
-  document.getElementById('iframe').src = 'html/lock.html';
-}
-
-function unknownPopup(){
-  document.getElementById('iframe').src = 'html/unknown.html';
-}
-
 function startCalcScifi(){
   calc(50,"scifi-stats");
 }
