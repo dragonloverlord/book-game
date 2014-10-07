54738c5a321e64226987ef714a5db75f5bb60b91
dragonloverlord <dragonloverlord@gmail.com>
2014-10-07 12:51:12 -0400

    added context menu

--- main.js
+++ main.js
@@ -6,3 +6,23 @@ chrome.app.runtime.onLaunched.addListener(function() {
     }
   });
 });
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({
+    title: 'Help',
+    id: 'help',
+    contexts: [ 'all' ]
+  });
+  chrome.contextMenus.create({
+    title: 'About This Game',
+    id: 'about',
+    contexts: [ 'all' ]
+  });
+});
+
+chrome.contextMenus.onClicked.addListener(function(itemData) {
+	if (itemData.menuItemId == "help")
+		chrome.app.window.create('html/help.html', {bounds:{width: 600, height: 300}});
+	if (itemData.menuItemId == "about")
+		chrome.app.window.create('html/about.html', {bounds:{width: 600, height: 300}});
+});
