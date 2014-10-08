3179c8c2d5a8b0bfad08f555c4192e962b51e549
dragonloverlord <dragonloverlord@gmail.com>
2014-10-08 10:52:04 -0400

    added general script

---
+++ js/help.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector("#general").addEventListener('click',general);
+});
+
+function general(){
+  document.getElementById('iframe').src = 'html/general.html';
+}
