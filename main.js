d98ae0cc62c3df413dc0cd24fddb27f2ea91da89
dragonloverlord <dragonloverlord@gmail.com>
2014-12-16 09:11:18 -0500

    commented out update warning.

--- main.js
+++ main.js
@@ -44,6 +44,7 @@ chrome.contextMenus.onClicked.addListener(function(itemData) {
 		});
 });
 
+/**
 chrome.runtime.onInstalled.addListener(function(details){
   if(details.reason == "update"){
     chrome.app.window.create('html/update.html', {
@@ -58,3 +59,4 @@ chrome.runtime.onInstalled.addListener(function(details){
     });
   }
 });
+**/
