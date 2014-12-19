e0d02ea1db92cce0829e8cea96fc43ca0660fc9c
dragonloverlord <dragonloverlord@gmail.com>
2014-12-19 08:27:04 -0500

    moved id to appropriate place.

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
+    id: 'mainWindow',
     innerBounds: {
       width: 1200,
       height: 658,
       minWidth: 1200,
       minHeight: 658
-    },
-    id: 'mainWindow'
+    }
   });
 });
 
