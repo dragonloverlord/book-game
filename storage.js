559300c88fcf05974dc70695212b3986dd439b27
dragonloverlord <dragonloverlord@gmail.com>
2014-12-19 09:08:52 -0500

    added actionLockValue.

--- storage.js
+++ storage.js
@@ -1,3 +1,4 @@
 chrome.storage.local.set({"trashyLockValue":"1"});
 chrome.storage.local.set({"historyLockValue":"1"});
 chrome.storage.local.set({"romanceLockValue":"1"});
+chrome.storage.local.set({"actionLockValue":""});
