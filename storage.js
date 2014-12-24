44c60d1ac7126464da8746ddfad5956d12733eb4
dragonloverlord <dragonloverlord@gmail.com>
2014-12-24 08:42:13 -0500

    added religiousLockValue storage variable.

--- storage.js
+++ storage.js
@@ -4,3 +4,4 @@ chrome.storage.local.set({"romanceLockValue":"1"});
 chrome.storage.local.set({"actionLockValue":"1"});
 chrome.storage.local.set({"environmentalLockValue":"1"});
 chrome.storage.local.set({"parentingLockValue":"1"});
+chrome.storage.local.set({"religiousLockValue":"1"});
