a5ae5ea703a9a2096ad88a69ac3135d97a055f63
dragonloverlord <dragonloverlord@gmail.com>
2015-01-05 10:47:06 -0500

    removed remaining content and flagged as unused.

--- js/error.js
+++ js/error.js
@@ -1,8 +1,1 @@
-setTimeout(lockCheck, 4000);
-function lockCheck(){
-  var humorStats = gt("humor-stats").innerHTML;
-  var humorStats = parseInt(humorStats);
-  if(humorStats >= 5){
-    unlockUnknown("Satire","satire","satire-sn");
-  }
-}
+//Not used
