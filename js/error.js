2e11662eb907e09e62f1bf2390089b702991c20d
dragonloverlord <dragonloverlord@gmail.com>
2015-01-05 09:32:14 -0500

    removed scifi stats checker.

--- js/error.js
+++ js/error.js
@@ -1,12 +1,8 @@
 setTimeout(lockCheck, 4000);
 function lockCheck(){
-  var scifiStats = gt("scifi-stats").innerHTML;
-  var scifiStats = parseInt(scifiStats);
   var humorStats = gt("humor-stats").innerHTML;
   var humorStats = parseInt(humorStats);
-  if(scifiStats >= 10){
-    unlockUnknown("Fantasy","fantasy","fantasy-sn");
-  }if(humorStats >= 5){
+  if(humorStats >= 5){
     unlockUnknown("Satire","satire","satire-sn");
   }
 }
