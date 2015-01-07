7519181b7e4ee4358bb74cb705f277723d321497
dragonloverlord <dragonloverlord@gmail.com>
2015-01-07 08:36:52 -0500

    added educationalLockValue to chrome local storage.

--- storage.js
+++ storage.js
@@ -8,3 +8,4 @@ chrome.storage.local.set({"religiousLockValue":"1"});
 chrome.storage.local.set({"fantasyLockValue":"1"});
 chrome.storage.local.set({"satireLockValue":"1"});
 chrome.storage.local.set({"comicLockValue":"1"});
+chrome.storage.local.set({"educationalLockValue":""});
