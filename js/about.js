94999dabfd1f607be892b379f72894d2cb6c6338
dragonloverlord <dragonloverlord@gmail.com>
2015-05-23 15:00:45 -0400

    Moving some files around for dart.

--- js/about.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  manifest = chrome.runtime.getManifest();
-  document.getElementById('version').innerHTML = "book writer v " + manifest.version;
-});
