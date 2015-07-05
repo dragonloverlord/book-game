f6777507dcd86fefd1717043eba65cc7eb21bd52
dragonloverlord <dragonloverlord@gmail.com>
2015-07-05 15:41:49 -0400

    added file

---
+++ web/dart/history_stats.dart
@@ -0,0 +1,21 @@
+library main.index_maker.money_div.history_stats;
+
+import 'dart:html';
+
+Element _div_element = new Element.tag('div');
+
+
+class HistoryStats{
+    static void main(){
+        historyStats();
+    }
+    
+    static void historyStats(){
+        _div_element = new Element.tag('div');
+        _div_element.setAttribute('class', 'save-div');
+        _div_element.setAttribute('id', 'history-stats');
+        _div_element.setAttribute('ng-init', 'load.historybook()');
+        _div_element.text = '0';
+        querySelector('#money-div').append(_div_element);
+    }
+}
