2395b96b6cb877ba45fde65a580c85347c3fe7ba
dragonloverlord <dragonloverlord@gmail.com>
2015-08-06 00:34:50 -0400

    added file.

---
+++ web/dart/nonfiction_stats.dart
@@ -0,0 +1,21 @@
+library main.index_maker.money_div.nonfiction_stats;
+
+import 'dart:html';
+
+Element _div_element = new Element.tag('div');
+
+
+class NonFictionStats{
+    static void main(){
+        nonFictionStats();
+    }
+    
+    static void nonFictionStats(){
+        _div_element = new Element.tag('div');
+        _div_element.setAttribute('class', 'save-div');
+        _div_element.setAttribute('id', 'nonfiction-stats');
+        _div_element.setAttribute('ng-init', 'load.nonfiction()');
+        _div_element.text = '0';
+        querySelector('#money-div').append(_div_element);
+    }
+}
