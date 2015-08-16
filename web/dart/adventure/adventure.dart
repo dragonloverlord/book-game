d984ac76011c2268ae9e589c48ec6eb7e6d63b42
dragonloverlord <dragonloverlord@gmail.com>
2015-08-16 19:10:04 -0400

    added file

---
+++ web/dart/adventure/adventure.dart
@@ -0,0 +1,12 @@
+library main.index_maker.money_div.adventure;
+
+import 'adventure_sn.dart';
+import 'adventure_stats.dart';
+
+
+class Adventure{
+    static void moneyDiv(){
+        AdventureSn.main();
+        AdventureStats.main();
+    }
+}
