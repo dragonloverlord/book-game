ba211fe125792270c8471735109b2d75d3060d33
dragonloverlord <dragonloverlord@gmail.com>
2015-08-16 19:27:07 -0400

    added file

---
+++ web/dart/childrens/childrens.dart
@@ -0,0 +1,12 @@
+library main.index_maker.money_div.childrens;
+
+import 'childrens_sn.dart';
+import 'childrens_stats.dart';
+
+
+class Childrens{
+    static void moneyDiv(){
+        ChildrensSn.main();
+        ChildrensStats.main();
+    }
+}
