638541ce331cd1ebff5f54bc945e1111449d99b3
dragonloverlord <dragonloverlord@gmail.com>
2015-08-17 13:11:07 -0400

    added file

---
+++ web/dart/drama/drama.dart
@@ -0,0 +1,12 @@
+library main.index_maker.money_div.drama;
+
+import 'drama_sn.dart';
+import 'drama_stats.dart';
+
+
+class Drama{
+    static void moneyDiv(){
+        DramaSn.main();
+        DramaStats.main();
+    }
+}
