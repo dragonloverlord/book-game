9c43d42c714e3d9ae0626a55fdc3f7b9a4331058
dragonloverlord <dragonloverlord@gmail.com>
2015-08-20 15:03:35 -0400

    added file

---
+++ web/dart/gossip/gossip.dart
@@ -0,0 +1,12 @@
+library main.index_maker.money_div.gossip;
+
+import 'gossip_sn.dart';
+import 'gossip_stats.dart';
+
+
+class Gossip{
+    static void moneyDiv(){
+        GossipSn.main();
+        GossipStats.main();
+    }
+}
---
+++ web/dart/gossip/gossip_sn.dart
@@ -0,0 +1,20 @@
+library main.index_maker.money_div.gossip.gossip_sn;
+
+import 'dart:html';
+
+Element _div_element = new Element.tag('div');
+
+
+class GossipSn{
+    static void main(){
+        gossipSn();
+    }
+    
+    static void gossipSn(){
+        _div_element = new Element.tag('div');
+        _div_element.setAttribute('class', 'stats-text');
+        _div_element.setAttribute('id', 'gossip-sn');
+        _div_element.text = '???';
+        querySelector('#money-div').append(_div_element);
+    }
+}
