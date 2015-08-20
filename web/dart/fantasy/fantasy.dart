6ae347abc919705af76fceda8e6584b82094f153
dragonloverlord <dragonloverlord@gmail.com>
2015-08-20 03:12:26 -0400

    added file

---
+++ web/dart/fantasy/fantasy.dart
@@ -0,0 +1,12 @@
+library main.index_maker.money_div.fantasy;
+
+import 'fantasy_sn.dart';
+import 'fantasy_stats.dart';
+
+
+class Fantasy{
+    static void moneyDiv(){
+        FantasySn.main();
+        FantasyStats.main();
+    }
+}
