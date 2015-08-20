e83c12bdf94cf39b0f2203d9a6bc417692664b5c
dragonloverlord <dragonloverlord@gmail.com>
2015-08-20 02:56:47 -0400

    added file

---
+++ web/dart/environmental/environmental.dart
@@ -0,0 +1,12 @@
+library main.index_maker.money_div.environmental;
+
+import 'environmental_sn.dart';
+import 'environmental_stats.dart';
+
+
+class Environmental{
+    static void moneyDiv(){
+        EnvironmentalSn.main();
+        EnvironmentalStats.main();
+    }
+}
---
+++ web/dart/environmental/environmental_stats.dart
@@ -0,0 +1,21 @@
+library main.index_maker.money_div.environmental_stats;
+
+import 'dart:html';
+
+Element _div_element = new Element.tag('div');
+
+
+class EnvironmentalStats{
+    static void main(){
+        environmentalStats();
+    }
+    
+    static void environmentalStats(){
+        _div_element = new Element.tag('div');
+        _div_element.setAttribute('class', 'save-div');
+        _div_element.setAttribute('id', 'environmental-stats');
+        _div_element.setAttribute('ng-init', 'load.environmental()');
+        _div_element.text = '0';
+        querySelector('#money-div').append(_div_element);
+    }
+}
