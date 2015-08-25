645c4b020edae8a22dadeb7f954cf3e5bb841fee
dragonloverlord <dragonloverlord@gmail.com>
2015-08-25 15:48:29 -0400

    added file

---
+++ web/dart/humor/humor.dart
@@ -0,0 +1,12 @@
+library main.index_maker.money_div.humor;
+
+import 'humor_sn.dart';
+import 'humor_stats.dart';
+
+
+class Humor{
+    static void moneyDiv(){
+        HumorSn.main();
+        HumorStats.main();
+    }
+}
