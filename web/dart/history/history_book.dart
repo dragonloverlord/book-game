72b65a4b65213272afb6fea3da706a2c240dfc78
dragonloverlord <dragonloverlord@gmail.com>
2015-08-25 15:34:32 -0400

    added file

---
+++ web/dart/history/history_book.dart
@@ -0,0 +1,12 @@
+library main.index_maker.money_div.history;
+
+import 'history_sn.dart';
+import 'history_stats.dart';
+
+
+class HistoryBook{
+    static void moneyDiv(){
+        HistorySn.main();
+        HistoryStats.main();
+    }
+}
