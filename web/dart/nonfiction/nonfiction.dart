9204ca266f3cf1c9ea6d5d454eedc63c368c504c
dragonloverlord <dragonloverlord@gmail.com>
2015-08-27 02:54:06 -0400

    added file

---
+++ web/dart/nonfiction/nonfiction.dart
@@ -0,0 +1,12 @@
+library main.index_maker.money_div.nonfiction;
+
+import 'nonfiction_sn.dart';
+import 'nonfiction_stats.dart';
+
+
+class NonFiction{
+    static void moneyDiv(){
+        NonFictionSn.main();
+        NonFictionStats.main();
+    }
+}
---
+++ web/dart/nonfiction/nonfiction_sn.dart
@@ -0,0 +1,19 @@
+library main.index_maker.money_div.nonfiction.nonfiction_sn;
+
+import 'dart:html';
+
+Element _div_element = new Element.tag('div');
+
+class NonFictionSn{
+    static void main(){
+        nonFictionSn();
+    }
+    
+    static void nonFictionSn(){
+        _div_element = new Element.tag('div');
+        _div_element.setAttribute('class', 'stats-text');
+        _div_element.setAttribute('id', 'nonfiction-sn');
+        _div_element.text = 'NonFiction';
+        querySelector('#money-div').append(_div_element);
+    }
+}
